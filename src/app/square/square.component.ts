ee3fbc9f5c65a36f8edeec62da7ded959b2fc277
oumayma987 <chaabanioumayma.2@gmail.com>
2020-01-28 15:58:58 +0100

    tictac

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -8,9 +8,5 @@ import { Component, Output, Input, EventEmitter } from '@angular/core';
 export class SquareComponent {
   @Input() value: string;
   @Input() winColor: boolean;
-  @Output() click = new EventEmitter<string>();
 
-  handleClick() {
-    this.click.emit();
-  }
 }
